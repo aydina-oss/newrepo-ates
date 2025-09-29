66cac94fe2cef485013ca25240f9b60395c65955
aydina-oss <aydina@stpaulsschool.org.uk>
2025-09-29 10:39:21 +0100

    k

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 using System;
 
 class Program
@@ -8,6 +7,25 @@ class Program
     public static void Main(string[] args)
     {
 
+
+    }
+
+
+    static String mexicanwave(string input)
+    {
+        String result = "";
+        for (int i = 0; i < input.Length; i++)
+        {
+            if (input.Substring(i, 1).Equals(" "))
+            {
+                continue;
+            }
+            else
+            {
+                String x = input.Substring(i, 1).ToUpper;
+            }
+        }
+        
     }
 
 
