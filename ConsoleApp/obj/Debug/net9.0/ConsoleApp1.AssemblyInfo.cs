ea262b5be5ee0eec5f01ddccaed47165e43173f7
aydina-oss <aydina@stpaulsschool.org.uk>
2025-10-08 09:02:00 +0100

    ok

--- ConsoleApp/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c93021ee471d53701c4e2b1be7f59540793c40c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8561d64ac961152f095ec1dae28a1637d0ec6809")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
